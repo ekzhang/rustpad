f01b0181db8f9c0fa5742736a97dcd87b231a89a
Eric Zhang <ekzhang1@gmail.com>
2021-05-31 12:09:30 -0500

    Add starter code for warp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use warp::{filters::BoxedFilter, Filter, Reply};
+
+fn server() -> BoxedFilter<(impl Reply,)> {
+    (warp::path!("hello" / String).map(|name| format!("Hello, {}!", name)))
+        .or(warp::path::end().map(|| "Home page"))
+        .boxed()
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+
+    let port = std::env::var("PORT")
+        .unwrap_or("3030".to_string())
+        .parse()
+        .expect("Unable to parse PORT");
+
+    println!("Server listening on http://localhost:{}", port);
+    warp::serve(server()).run(([0, 0, 0, 0], port)).await;
 }
