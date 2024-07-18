1e95c079b8546420ab13a9a114d6a762aa4a377c
Eric Zhang <ekzhang1@gmail.com>
2024-07-18 14:21:08 -0400

    Update Vite to v5 and fix proxy target to ipv4

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3030",
+        target: "http://127.0.0.1:3030",
         changeOrigin: true,
         secure: false,
         ws: true,
