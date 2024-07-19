ea2229f1c741637d5ba1e5cba2cf1d55eb893dd2
Erin Yuki Schlarb <erin-dev@ninetailed.ninja>
2024-07-19 15:35:54 +0200

    Accept document ID also as last pathname component and move it to the fragment in the displayed URL if received like this

--- src/useHash.ts
+++ src/useHash.ts
@@ -5,11 +5,22 @@ const idLen = 6;
 
 function getHash() {
   if (!window.location.hash) {
-    let id = "";
-    for (let i = 0; i < idLen; i++) {
-      id += chars[Math.floor(Math.random() * chars.length)];
+    let newUrl = new URL(window.location);
+
+    // Attempt retriving document ID as last part of path, moving it to the fragment
+    let filename = newUrl.pathname.split("/").pop();
+    if (filename != "" && filename != "index.html") {
+      newUrl.pathname = newUrl.pathname.slice(0, newUrl.pathname - filename.length);
+      newUrl.hash = filename;
+    } else {
+      // Generate random document ID as fallback / direct-hit
+      let id = "";
+      for (let i = 0; i < idLen; i++) {
+        id += chars[Math.floor(Math.random() * chars.length)];
+      }
+      newUrl.hash = id;
     }
-    window.history.replaceState(null, "", "#" + id);
+    window.history.replaceState(null, "", newUrl.href);
   }
   return window.location.hash.slice(1);
 }
