4247aa1d1f80790d8a84fd61c54e86c3eaab7eb3
Erin Yuki Schlarb <erin-dev@ninetailed.ninja>
2024-12-31 02:14:01 +0000

    Update app to React 18 + Chakra 2 + Framer-Motion 11 (#83)
    * Update app to React 18 + Chakra 2 + Framer-Motion 11
    
    Animation support needs a newer Framer-Motion version which pulls in newer
    React which again is only supported by newer Chakra.
    
    * Switch to `ReactDOM.createRoot`
    
    The old `ReactDOM.render` function is deprecated in React 18.
    
    See: https://reactjs.org/link/switch-to-createroot

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import * as wasm from "rustpad-wasm";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <StrictMode>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
